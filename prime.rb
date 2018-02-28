fa92782a58c69a01e2eb649ceafe0d4515997264
Joseph DiPeppe <jkdipeppe@gmail.com>
2018-02-28 19:05:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(int)
+  if int < 2
+    return false
+  else
+    for i in 2..int 
+      if int % i == 0
+        return false
+      end
+    end
+  end
+end# Add  code here!
